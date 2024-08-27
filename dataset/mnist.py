8222e3c0becb8c31968d34d0e01ed5204be6c866
Jing Zhang <jing3jour@gmail.com>
2024-08-27 21:51:37 +0200

    update

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -10,8 +10,8 @@ import os
 import numpy as np
 
 
-url_base = 'http://yann.lecun.com/exdb/mnist/'
-#url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'  # mirror site
+#url_base = 'http://yann.lecun.com/exdb/mnist/'
+url_base = 'https://ossci-datasets.s3.amazonaws.com/mnist/'  # mirror site
 
 key_file = {
     'train_img':'train-images-idx3-ubyte.gz',
